69ffd2d3e2505e7b19dbbd22a056b70b47847c41
parvans <parvansajeevan@gmail.com>
2023-06-23 13:06:53 +0530

    add auth to the userprofile route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { getUserProfile, getallUsers, login, resetPassword, signup, verifyCode, verifyEmail } from '../controllers/user-controller.js'
+import auth from '../middleware/auth.js'
 const router = express.Router()
 router.get('/getallusers', getallUsers)
 router.post('/usersignup',signup)
@@ -7,5 +8,5 @@ router.post('/userlogin', login)
 router.post('/verifyemail',verifyEmail)
 router.post('/verifyotp',verifyCode)
 router.post('/resetpassword',resetPassword)
-router.get('/userprofile',getUserProfile)
+router.get('/userprofile',auth,getUserProfile)
 export default router
