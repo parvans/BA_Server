288e214b24a65da38aabfbc1a6c60310bb8ff877
parvans <parvansajeevan@gmail.com>
2023-06-28 17:15:14 +0530

    ups

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,8 +1,7 @@
 import { Blog } from "../models/Blog.js";
-import { Draft } from "../models/Draft.js";
 import { User } from "../models/User.js";
 import cloudNary from "../utils/cloudinary.js";
-   
+
 export const getallBlogs = async (req, res) => {
     let blogs;
     try {
@@ -17,21 +16,52 @@ export const getallBlogs = async (req, res) => {
         return res.status(200).json({ data: blogs });
     }
 }
+export const usersBlogs = async (req, res) => {
+    const userId=req.user.id
+    let userBlogs;
+    let filterBlogs;
+    try{
+        userBlogs=await User.findById({_id:userId}).select('blogs').populate('blogs').sort({createdAt:-1})
+        filterBlogs=userBlogs.blogs.filter((blog)=>blog.isDraft===false && blog.isTrash===false)
+    }catch(err){
+        return console.log(err);
+    }
+    if(!userBlogs){
+        return res.status(404).json({message:"No Blogs Found"})
+    }
+    return res.status(200).json({data:filterBlogs.reverse()})
+}
+export const getByid = async (req, res) => {
+    const blogId=req.query.id
+    let blog;
+    try{
+        blog=await Blog.findById(blogId).populate({path:'userId',select:'name email'})
+    }catch(err){
+        console.log(err);
+    }
+    if(!blog){
+        return res.status(404).json({message:'No Blog Found'})
+    }else{
+        return res.status(200).json({blog})
+    }
+}
+
+
+
+
 
 export const addBlog = async (req, res, next) => {
-    const {title,description}=req.body
+    const {title,description,isDraft}=req.body
     const image=req.body.data
     const userId=req.user.id
     let existingUser;
     try{
-        
         existingUser=await User.findById({_id:userId})
-        
+        if(!existingUser){
+            return res.status(400).json({message:"Unable to find the user with this id"})
+        }
     }catch(err){
         return console.log(err);
-    }
-    if(!existingUser){
-        return res.status(400).json({message:"Unable to find the user with this id"})
     }
     // const theDesc=description.split(' ').map((item) => item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()).join(" ");
     const theTitle=title.split(' ').map((item) => item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()).join(" ");
@@ -44,6 +74,7 @@ export const addBlog = async (req, res, next) => {
         title:theTitle,
         description:description,
         image:uploadResponse.public_id,
+        isDraft,
         userId
     })
     try{
@@ -77,20 +108,6 @@ export const updateBlog = async (req, res) => {
 
 }
 
-export const getByid = async (req, res) => {
-    const blogId=req.query.id
-    let blog;
-    try{
-        blog=await Blog.findById(blogId).populate({path:'userId',select:'name email'})
-    }catch(err){
-        console.log(err);
-    }
-    if(!blog){
-        return res.status(404).json({message:'No Blog Found'})
-    }else{
-        return res.status(200).json({blog})
-    }
-}
 
 export const deleteBlog = async (req, res) => {
     const blogId=req.query.id
@@ -100,8 +117,8 @@ export const deleteBlog = async (req, res) => {
         if(!blog){
             return res.status(404).json({message:'No Blog Found'})
         }else{
-            await Blog.findByIdAndDelete(blogId)
             await cloudNary.uploader.destroy(blog.image)
+            await Blog.findByIdAndDelete(blogId)
             await User.findByIdAndUpdate(blog.userId,{$pull:{blogs:blogId}})
             return res.status(200).json({message:'Blog Successfully Deleted'})
         }
@@ -110,140 +127,70 @@ export const deleteBlog = async (req, res) => {
     }
 }
 
-export const usersBlogs = async (req, res) => {
-    const userId=req.user.id
-    let userBlogs;
-    try{
-        userBlogs=await User.findById({_id:userId, isDraft: false, isTrash: false }).select('blogs').populate('blogs').sort({createdAt:-1})
-    }catch(err){
-        return console.log(err);
-    }
-    if(!userBlogs){
-        return res.status(404).json({message:"No Blogs Found"})
-    }
-    return res.status(200).json({data:userBlogs.blogs.reverse()})
-}
-
-export const saveToDraft=async(req,res)=>{
-    const {title,description}=req.body
-    const image=req.body.data
-    const userId=req.user.id
-    let existingUser;
-    try{
-        
-        existingUser=await User.findById({_id:userId})
-        
-    }catch(err){
-        return console.log(err);
-    }
-    if(!existingUser){
-        return res.status(400).json({message:"Unable to find the user with this id"})
-    }
-    // const theDesc=description.split(' ').map((item) => item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()).join(" ");
-    const theTitle=title.split(' ').map((item) => item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()).join(" ");
-    const exDraft=await Draft.findOne({title:theTitle})
-    if(exDraft){
-        return res.status(400).json({message:"Blog with this title already exists in the draft"})
-    }
-    const uploadResponse = await cloudNary.uploader.upload(image,{upload_preset: 'blog_images'})
-    const newDraft=new Draft({
-        title:theTitle,
-        description:description,
-        image:uploadResponse.public_id,
-        userId
-    })
-    try{
-        await newBlog.save()
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({message:err})
-    }
-    return res.status(200).json({message:"Blog added to Draft successfully"})
-}
-
-export const getDrafts=async(req,res)=>{
-    const userId=req.user.id
-    let drafts;
-    try{
-        drafts=await Draft.find({userId})
-    }catch(err){
-        return console.log(err);
-    }
-    if(!drafts){
-        return res.status(404).json({message:"No Drafts Found"})
-    }
-    return res.status(200).json({data:drafts})
-}
-
-export const deleteDraft=async(req,res)=>{
-    const draftId=req.query.id
-    let draft;
-    try{
-        draft=await Draft.findById(draftId)
-        if(!draft){
-            return res.status(404).json({message:'No Draft Found'})
+export const moveToTrash = async (req, res) => {
+    const blogId=req.query.id
+    let blog;
+    try {
+        blog=await Blog.findById(blogId)
+        if(!blog){
+            return res.status(404).json({message:'No Blog Found'})
         }else{
-            await Draft.findByIdAndDelete(draftId)
-            await cloudNary.uploader.destroy(draft.image)
-            return res.status(200).json({message:'Draft Successfully Deleted'})
+            await Blog.findByIdAndUpdate(blogId,{isTrash:true,isDraft:false})
+            return res.status(200).json({message:'Blog Successfully Moved to Trash'})
         }
-    }catch(err){
+    } catch (error) {
         console.log(err);
     }
 }
 
-export const getADraft=async(req,res)=>{
-    const draftId=req.query.id
-    let draft;
+export const moveToDraft=async(req,res)=>{
+    const blogId=req.query.id
+    let blog;
     try {
-        draft=await Draft.findById(draftId)
-    } catch (error) {
-        console.log(error);
-    }
-    if(!draft){
-        return res.status(404).json({message:"No Draft Found"})
-    }else{
-        return res.status(200).json({data:draft})
-    }
-}
-export const updateDraft=async(req,res)=>{
-    const {title,description}=req.body
-    const draftId=req.query.id
-    try {
-    const draftExist=await Draft.findById(draftId)
-    if(!draftExist){
-        return res.status(404).json({message:"No Draft Found"})
-    }
-    await Draft.findByIdAndUpdate(draftId,{
-        title,
-        description
-    },{new:true})
-    res.status(200).json({message:"Draft updated successfully"})
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-export const publishDraft=async(req,res)=>{
-    const draftId=req.query.id
-    let draft;
-    try {
-        draft=await Draft.findById(draftId)
-        if(!draft){
-            return res.status(404).json({message:"No Draft Found"})
+        blog=await Blog.findById(blogId)
+        if(!blog){
+            return res.status(404).json({message:'No Blog Found'})
+        }else{
+            await Blog.findByIdAndUpdate(blogId,{isTrash:false,isDraft:true})
+            return res.status(200).json({message:'Blog Successfully Moved to Draft'})
         }
-        const newBlog=new Blog({
-            title:draft.title,
-            description:draft.description,
-            image:draft.image,
-            userId:draft.userId
-        })
-        const result=await newBlog.save()
-        const theUser=await User.findById(draft.userId)
-        theUser.blogs.push(result._id)
-        await Draft.findByIdAndDelete(draftId)
-
     } catch (error) {
-        console.log(error);
+        console.log(err);
     }
 }
+
+export const getUserDraftBlogs = async (req, res) => {
+    const userId=req.user.id
+    let blogs;
+    let draftBlogs;
+    try {
+        blogs = await User.findById({_id:userId,}).select('blogs').populate('blogs').sort({ createdAt: -1 });
+        draftBlogs=blogs.blogs.filter((blog)=>blog.isDraft===true)
+    } catch (err) {
+        console.log(err);
+    }
+    if (!blogs) {
+        return res.status(404).json({ message: "No Blogs Found" });
+    } else {
+        return res.status(200).json({ data: draftBlogs });
+    }
+}
+
+export const getUserTrashBlogs = async (req, res) => {
+    const userId=req.user.id
+    let blogs;
+    let trashBlogs;
+    try {
+        blogs = await User.findById({_id:userId,}).select('blogs').populate('blogs').sort({ createdAt: -1 });
+        trashBlogs=blogs.blogs.filter((blog)=>blog.isTrash===true)
+    } catch (err) {
+        console.log(err);
+    }
+    if (!blogs) {
+        return res.status(404).json({ message: "No Blogs Found" });
+    } else {
+        return res.status(200).json({ data: trashBlogs });
+    }
+}
+
+
