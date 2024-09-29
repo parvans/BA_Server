8869c2860732d0ec882d4129a8732a0c978ebd23
parvan <parvansajeevan@gmail.com>
2024-09-29 16:49:05 +0530

    ./

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 const dataBase = () => {
     try {
     // mongoose.set('strictQuery', false);  
-        mongoose.connect(process.env.MONGODB_URI_CLOUD)
+        mongoose.connect(`mongodb+srv://parvan:${process.env.MONGODB_URI_CLOUD}@cluster0.14wed.mongodb.net/?retryWrites=true&w=majority`)
             .then(() => console.log("Connected to MongoDB Cloud Atlas 🚀"))
             .catch((err) => console.error(err));
     } catch (error) {
